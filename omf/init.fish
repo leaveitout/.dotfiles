37cb990f2664d4e81db954648731a11705de482a
Seán Bruton <sbruton@tcd.ie>
2017-02-24 10:48:05 +0000

    Some new fish scripts.

--- omf/init.fish
+++ omf/init.fish
@@ -127,6 +127,14 @@ function gigls
   curl -L -s https://www.gitignore.io/api/list
 end
 
+function tro
+  tree -L 1 $argv
+end
+
+function trt
+  tree -L 2 $argv
+end
+
 # Base16 Shell
 eval sh /home/sean/Documents/libraries/base16-shell/base16-tomorrow.dark.sh
 
