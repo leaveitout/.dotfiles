f4356362c7ed1f3a9f18c626ef08069952ef1822
First Second <changethis@google.com>
2020-02-04 11:04:14 +0000

    Updated files for changes in upstream

--- omf/before.init.fish
+++ omf/before.init.fish
@@ -2,5 +2,6 @@ source /usr/share/autojump/autojump.fish
 
 # Source keychain
 if status --is-interactive
-  keychain --eval --quiet -Q id_rsa
+#   keychain --eval --quiet -Q ~/.ssh/id_rsa
+    keychain --quiet --agents ssh github
 end
